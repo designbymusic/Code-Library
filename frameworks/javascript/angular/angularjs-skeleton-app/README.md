ca6887400ba742b9eefb4e64ecc72da9936d49d3
Dave Ashman <public@iamdash.com>
2013-07-15 17:31:53 +0100

    halfway through Building a Web App From Scratch in AngularJS

---
+++ frameworks/javascript/angular/angularjs-skeleton-app/README.md
@@ -0,0 +1,2 @@
+#Building a Web App From Scratch in AngularJS Tutorial
+http://net.tutsplus.com/tutorials/javascript-ajax/building-a-web-app-from-scratch-in-angularjs/
--- frameworks/javascript/angular/angularjs-skeleton-app/js/tv-premieres-app/controllers/mainController.js
+++ frameworks/javascript/angular/angularjs-skeleton-app/js/tv-premieres-app/controllers/mainController.js
@@ -1,7 +1,15 @@
 app.controller("mainController", function($scope, $http){
-
+    $scope.apiKey = "b91d3d0920149810f22ede8dc2ba8c90";
     $scope.init = function() {
+        //API requires a start date
+        var today = new Date();
+        //Create the date string and ensure leading zeros if required
+        var apiDate = today.getFullYear() + ("0" + (today.getMonth() + 1)).slice(-2) + "" + ("0" + today.getDate()).slice(-2);
+        $http.jsonp('http://api.trakt.tv/calendar/premieres.json/' + $scope.apiKey + '/' + apiDate + '/' + 30 + '/?callback=JSON_CALLBACK').success(function(data) {
+            console.log(data);
+        }).error(function(error) {
 
+        });
     };
 
 });
